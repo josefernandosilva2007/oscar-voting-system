d8a35d84a05f22bd57b9157d945a89fb3422dd3c
Jose Fernando <josefernandosilva0501@gmail.com>
2025-11-19 13:26:04 -0300

    initial commit

---
+++ src/main/java/com/fear/oscar_voting_system/OscarVotingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fear.oscar_voting_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscarVotingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscarVotingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fear/oscar_voting_system/OscarVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fear.oscar_voting_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscarVotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
